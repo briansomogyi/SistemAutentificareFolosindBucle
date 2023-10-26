8c0d3ceeadc70351ef418d6bfb4fc8639449823a
briansomogyi <brian.somogyi@emanuel.ro>
2023-10-26 22:09:21 +0300

    Done some changes

---
+++ SistemAutentificareFolosindBucle/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace SistemAutentificareFolosindBucle
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            const string userName = "Brian";
+            const string password = "info_eco";
+            do 
+            {
+                Console.WriteLine("Introduceti userul si parola");
+                string userNameInput = Console.ReadLine();
+                string passwordInput = Console.ReadLine();
+                if (userName == userNameInput && password == passwordInput)
+                {
+                    Console.WriteLine($"Bine ai venit, {userName}");
+                    break;
+                }
+                else
+                {
+                    Console.WriteLine("Autentificare esuata");
+                }
+            } while (true);
+        }
+    }
+}
